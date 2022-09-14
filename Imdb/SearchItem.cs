7215977022592f49254545811c2f4ddb88a9557e
0mar-rivero <omarrivgw@gmail.com>
2022-09-14 11:44:02 -0400

    Almost done.

---
+++ Imdb/SearchItem.cs
@@ -0,0 +1,17 @@
+﻿namespace Imdb;
+
+public record SearchItem(string Title, string Year, string imdbID, string Type, string Poster);
+
+public record SearchResult(SearchItem[] Search, string totalResults, string Response);
+
+public record ItemInfo(string Title, string Year, string Rated, string Released, string Runtime, string Genre,
+	string Director, string Writer, string Actors, string Plot, string Language, string Country, string Awards,
+	string Poster, Rating[] Ratings, string Metascore, string imdbRating, string imdbVotes, string imdbID, string Type,
+	string DVD, string BoxOffice, string Production, string Website, string Response) {
+
+	public static ItemInfo Empty => new("", "", "", "", "", "", "", "", "", "", 
+		"", "", "", "", Array.Empty<Rating>(), "", "", "",
+		"", "", "", "", "", "", "");
+}
+
+public record Rating(string Source, string Value);
---
+++ ImdbBot/InLineHandling.cs
@@ -0,0 +1,46 @@
+﻿using System.Security.Authentication;
+using Imdb;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.InlineQueryResults;
+using Telegram.Bot.Types.ReplyMarkups;
+using File = System.IO.File;
+using static Imdb.Tools;
+
+namespace ImdbBot;
+
+static class InLineHandling {
+	public static async Task BotOnInLineQueryReceived(this ITelegramBotClient bot, InlineQuery inlineQuery) {
+		if (inlineQuery.Query.Length < 2) return;
+		var results = new List<InlineQueryResult>();
+		var searchResults = (await Tools.Search(inlineQuery.Query)).Search ?? Array.Empty<SearchItem>();
+		foreach (var item in searchResults) {
+			results.Add(new InlineQueryResultArticle($"{item.imdbID}", item.Title,
+				new InputTextMessageContent(await VideoMessageGenerator(item))) {
+				Description = $"{item.Type} ({item.Year})",
+				ThumbUrl = item.Poster,
+				ReplyMarkup = InlineKeyboardButton.WithUrl("IMDB","https://www.imdb.com/title/"+item.imdbID)
+			});
+		}
+
+		await bot.AnswerInlineQueryAsync(inlineQuery.Id, results);
+	}
+
+	public static async Task BotOnChosenInlineResultReceived(this ITelegramBotClient botClient,
+		ChosenInlineResult chosenInlineResult) {
+		if (chosenInlineResult.InlineMessageId is not null)
+			await botClient.EditMessageCaptionAsync(chosenInlineResult.InlineMessageId, await VideoMessageGenerator(chosenInlineResult.ResultId));
+	}
+
+	private static async Task<string> VideoMessageGenerator(string imdbID) {
+		var info = await GetInfo(imdbID);
+		return $"{info.Title}•{info.Type}\n" +
+		       $"{info.Runtime} ⭐️ {info.imdbRating} IMBD\n\n" +
+		       $"Directors: {info.Director}\n" +
+		       $"Actors: {info.Actors}\n\n" +
+		       $"Genres: {info.Genre}\n" +
+		       $"{info.Plot}";
+	}
+
+	private static async Task<string> VideoMessageGenerator(SearchItem item) => await VideoMessageGenerator(item.imdbID);
+}
